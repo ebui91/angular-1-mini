dd55aa3cb49f51ada7a3af7d9327f9c4b8ec83c8
Eric Bui <ericbui2@gmail.com>
2017-12-05 15:27:09 -0600

    fixed callback function in my controller

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,7 +2,7 @@ angular.module("friendsList").controller("mainCtrl", function($scope){
   $scope.test= "Angular MINI Project";
   $scope.friends= ["friend1", "friend2", "friend3", "friend4", "friend5"];
   $scope.input="";
-  $scope.addFriend= newFriend=> {
+  $scope.addFriend= () => {
     $scope.friends.push($scope.input);
     $scope.input="";
   }
